dad927a4c3ee3db3b580e8cdf310da040047ed84
IVaNaRsenoV <37076298+IVaNaRsenoV@users.noreply.github.com>
2022-06-06 15:38:04 +0300

    feat: Add basic empy template for graphql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(` type Default { id: ID! }`),
+    rootValue: {},
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => console.log(`Server works ${PORT}`));
